fbdd0a961edcc5b92daf78fd61b8cc166da54eac
AlCode88 <alanburkanov@gmail.com>
2022-10-09 22:16:23 -0500

    modified git flow

--- git.sh
+++ git.sh
@@ -7,7 +7,7 @@ gitBranch=$(git status | grep -Ewo 'Changes not staged')
 if [[ $gitBranch == 'Changes not staged' ]]
 then
 git add .
-read -p 'Enter your commit message: ' COMMIT
+read -p 'Enter your commit message without single/double quotes: ' COMMIT
 git commit -m "${COMMIT}"
 git push
 else
