5f93842faf9c27f1cf212c9c9f4674e9582ad5f9
AlCode88 <alanburkanov@gmail.com>
2022-10-09 21:49:52 -0500

    added files

--- git.sh
+++ git.sh
@@ -2,13 +2,12 @@
 
 gitBranch=git status | grep -E 'Your branch is up to date'
 
-if [[ $gitBranch == '0' ]]
+if [[ $gitBranch -eq '0' ]]
 then
     echo "Nothing to commit it is up to date"
+else
+git add .
+read -p 'Enter your commit message: ' COMMIT
+git commit -m "${COMMIT}"
+git push 
 fi
-#git add .
-
-#read -p 'Enter your commit message: ' COMMIT
-#git commit -m "${COMMIT}"
-
-#git push 
